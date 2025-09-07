f6e0a93b971310313c124bad0170491d844faf43
Sylvia Suet Wai Yung <sylvia.yung@ntnu.no>
2025-09-07 14:18:26 +0200

    chore: set up architecture

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
 
-function App() {
+export const App = () => {
   const [count, setCount] = useState(0)
 
   return (
@@ -29,7 +29,5 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
-
-export default App
+  );
+};

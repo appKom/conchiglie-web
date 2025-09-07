bf58dd1480204692eff5a21659a8d0c2f3330214
Sylvia Yung <160873928+SylviaSWYung@users.noreply.github.com>
2025-09-07 20:20:10 +0200

    feat: add button (#6)

---
+++ frontend/src/components/LinkButton.tsx
@@ -0,0 +1,28 @@
+import type { ReactNode } from "react";
+
+
+type color = "red" | "blue" | "green" | "yellow"
+
+interface ButtonProps{
+  children: ReactNode;
+  link: string; 
+  color: color;
+}
+
+const colorMap = new Map<color, string>([
+  ["green", "bg-dotgreen"],
+  ["blue", "bg-dotblue"],
+  ["red", "bg-dotred"],
+  ["yellow", "bg-dotyellow"]
+]);
+
+export const LinkButton = (props: ButtonProps) => {
+  return(
+    <a 
+      className={"text-4xl font-medium text-white w-xs p-4 text-center shadow-[-10px_-10px_0px_0px_rgba(80,80,80,1)] " + colorMap.get(props.color)}
+      href={props.link}
+    >
+      <slot>{props.children}</slot>
+    </a>
+  );
+};

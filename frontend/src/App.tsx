a6eb95d8dd7cce2ce315c0132ad57bb9e04d5821
Sylvia Suet Wai Yung <sylvia.yung@ntnu.no>
2025-09-12 16:50:01 +0200

    feat: add aboutuspage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 
 import { Header } from './components/Header';
 import './index.css'
+import { AboutUsPage } from './pages/AboutUsPage';
 import { HomePage } from './pages/HomePage';
 
 export const App = () => {
@@ -9,6 +10,7 @@ export const App = () => {
     <>
       <Header/>
       <HomePage/>
+      <AboutUsPage/>
     </>
   );
 };

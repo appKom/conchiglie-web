c0e93e0dce2ede97338589e141e3bb8a8178f3cf
Sylvia Yung <160873928+SylviaSWYung@users.noreply.github.com>
2025-09-12 20:53:55 +0200

    feat: implement homepage (#7)
    * feat: implement logo
    
    * feat: homepage
    
    * feat: homepage + responsive
    
    * feat: update homepage
    
    * feat: add aboutuspage
    
    * feat: implement aboutuspage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 
+import { Header } from './components/Header';
 import './index.css'
+import { AboutUsPage } from './pages/AboutUsPage';
 import { HomePage } from './pages/HomePage';
 
 export const App = () => {
 
   return (
     <>
+      <Header/>
       <HomePage/>
+      <AboutUsPage/>
     </>
   );
 };

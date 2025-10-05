20d4928c2aafd6da138da0a765790ebcd1bb979f
Sylvia Yung <160873928+SylviaSWYung@users.noreply.github.com>
2025-10-05 16:01:59 +0200

    feat: implement footer (#18)
    * feat: implement footer
    
    * feat: implement footer
    
    * style: run prettier

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,5 @@
+import { Footer } from '../components/Footer';
 import { LinkButton } from '../components/LinkButton';
-import wave from '../assets/wave.png';
-import { Header } from '../components/Header';
-import { AboutUsPage } from './AboutUsPage';
 
 export const HomePage = () => {
   return (
@@ -168,29 +166,7 @@ export const HomePage = () => {
           </div>
         </div>
       </section>
-
-      {/* <Header />
-      <div className="relative flex flex-col justify-center items-center content-between h-screen gap-12">
-        <img
-          src={wave}
-          alt="wave background"
-          className="absolute right-0 top-0 w-1/3 object-cover z-0 md:w-1/6"
-        />
-        <p className="text-6xl pl-5 relative z-10 md:text-center md:p-0 md:text-7xl">
-          Helt nye karrieredager på <b>NTNU</b> <br /> fra{" "}
-          <b>
-            våren <br />
-            2026
-          </b>
-        </p>
-        <p className="text-4xl italic relative z-10 md:text-5xl">
-          Interessert?
-        </p>
-        <LinkButton link="/contact" color="green">
-          Kontakt oss
-        </LinkButton>
-      </div>
-      <AboutUsPage /> */}
+      <Footer />
     </>
   );
 };
